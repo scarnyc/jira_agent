12680369e7536edfc127579a8e381816eec19b0f
Will Scardino <47287222+scarnyc@users.noreply.github.com>
2025-03-24 15:46:45 -0400

    Add files via upload

---
+++ jira_agent.py
@@ -0,0 +1,25 @@
+import os
+from langchain.agents import AgentType, initialize_agent
+from langchain_community.agent_toolkits.jira.toolkit import JiraToolkit
+from langchain_community.utilities.jira import JiraAPIWrapper
+from langchain_google_genai import ChatGoogleGenerativeAI
+
+GEM_API_KEY = os.environ["GEM_API_KEY"]
+JIRA_USERNAME = os.environ["JIRA_USERNAME"]
+JIRA_INSTANCE_URL = os.environ["JIRA_INSTANCE_URL"]
+JIRA_CLOUD = os.environ["JIRA_CLOUD"]
+PROJECT_KEY = os.environ["PROJECT_KEY"]
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.0-flash-lite",
+    google_api_key=GEM_API_KEY,
+    temperature=0.1
+)
+jira = JiraAPIWrapper()
+toolkit = JiraToolkit.from_jira_api_wrapper(jira)
+
+agent = initialize_agent(
+    toolkit.get_tools(), llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)
+
+agent.run("make a new issue in project PW to remind me to make more fried rice")
